691b666c8f81101ebe05fe89e6fd669c565081c9
Matt-Daniaux <matthieudaniaux@gmail.com>
2023-01-08 13:54:40 +0100

    ex

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const ordered = array.sort(function (a, b) {
+        const currentYear = (new Date()).getFullYear()
+        
+        let yearOfDeathA = a.yearOfDeath || currentYear;
+        let yearOfDeathB = b.yearOfDeath || currentYear;
+        
+        let aPerson = yearOfDeathA - a.yearOfBirth;
+        let bPerson = yearOfDeathB - b.yearOfBirth;
+        
+        if (aPerson > bPerson) { 
+            return -1
+        } else{ 
+            return 1};  
+    });
+
+    console.table(ordered)
+    //console.table(ordered)
+    return ordered[0]
 
 };
+
+/*const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+findTheOldest(people)*/
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
